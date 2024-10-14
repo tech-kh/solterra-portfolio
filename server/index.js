a4df7ee653a6a9ef9b61c0712f36c66a43f9375b
tech-kinako <tech.kinako@gmail.com>
2024-10-14 14:51:09 +0900

    nodeサーバー環境構築

---
+++ server/index.js
@@ -0,0 +1,14 @@
+/* server.ts */
+
+import express from "express";
+
+const app = express();
+const port = 9000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.listen(port, () => {
+  console.log(`port ${port} でサーバー起動中`);
+});
