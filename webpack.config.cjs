95fbe69c1c83a84a315e0f1b5a55f5764a235a5f
tech-kinako <tech.kinako@gmail.com>
2024-10-16 00:18:12 +0900

    babel導入

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,9 +3,24 @@ const path = require('path');
 module.exports = {
   target: 'node',
   mode: 'development',
-  entry: './server/index.js', 
+  entry: './server/index.js',
   output: {
-    path: __dirname + '/dist',
+    path: path.resolve(__dirname, 'dist'),
     filename: 'server.js',
-  }
+    chunkFormat: 'module'
+  },
+  experiments: {
+    outputModule: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };
